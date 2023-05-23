de18855431cf1ac43b9d9549177edc97bd521b82
Xavier Damman <xdamman@gmail.com>
2023-05-24 01:13:51 +0200

    updated readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
 
+const baseUrl = process.env.VERCEL_URL
+    ? "https://" + process.env.VERCEL_URL
+    : "http://localhost:3000";
+
 export default function Page() {
   return (
     <div>
@@ -14,7 +18,9 @@ export default function Page() {
         />
       </Head>
       <h1>Voucher generator</h1>
-      <p>See <a href="https://github.com/regensunite/vouchergenerator">https://github.com/regensunite/vouchergenerator</a> for more info.</p>
+      <p>Example:</p>
+      <img src={`${baseUrl}/api/voucher.png?contract_address=0x1234&goodfor=free%20cargobike%20ride&from=Xavier&date=2023-06-24`} />
+      <p>Documentation: <a href="https://github.com/regensunite/vouchergenerator">https://github.com/regensunite/vouchergenerator</a> for more info.</p>
     </div>
   )
 }
