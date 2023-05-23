c6a117ac130709f2554824e321006ba9320684b0
Xavier Damman <xdamman@gmail.com>
2023-05-24 00:57:17 +0200

    update index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,17 @@ export default function Page() {
   return (
     <div>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
+        <meta name="og:title" content="Voucher Generator" />
+        <meta name="og:description" content="Voucher Generator" />
         <meta
           name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
+          content={`${
               process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
-          }
+            }/voucher.jpg`}
         />
       </Head>
-      <h1>A page with Open Graph Image.</h1>
+      <h1>Voucher generator</h1>
+      <p>See <a href="https://github.com/regensunite/vouchergenerator">https://github.com/regensunite/vouchergenerator</a> for more info.</p>
     </div>
   )
 }
