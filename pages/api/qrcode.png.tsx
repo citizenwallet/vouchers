2b83d491022120ace5d50f7461f71a9fb157f9c8
Xavier Damman <xdamman@gmail.com>
2023-05-24 01:37:20 +0200

    fix

--- pages/api/qrcode.png.tsx
+++ pages/api/qrcode.png.tsx
@@ -5,6 +5,7 @@ export default async function handler(req, res) {
   console.log(req.query);
 
   const data = req.query.data;
+
   const qrlogodata = await QRLogo.generateQRWithLogo(data, "public/nft-logo.png", {}, "base64");
 
   res.setHeader('Content-Type', 'image/png');
