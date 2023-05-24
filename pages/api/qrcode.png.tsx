e993b4cc47631a6733776921ae07c75a76b34229
Xavier Damman <xdamman@gmail.com>
2023-05-24 02:14:02 +0200

    using url to load qrcode logo

--- pages/api/qrcode.png.tsx
+++ pages/api/qrcode.png.tsx
@@ -1,12 +1,16 @@
 import QRLogo from 'qr-with-logo';
 
+const baseUrl = process.env.VERCEL_URL
+  ? "https://" + process.env.VERCEL_URL
+  : "http://localhost:3000";
+
 export default async function handler(req, res) {
 
   console.log(req.query);
 
   const data = req.query.data;
 
-  const qrlogodata = await QRLogo.generateQRWithLogo(data, "public/nft-logo.png", {}, "base64");
+  const qrlogodata = await QRLogo.generateQRWithLogo(data, `${baseUrl}/nft-logo.png`, {}, "base64");
 
   res.setHeader('Content-Type', 'image/png');
   res.send(Buffer.from(qrlogodata, "base64"));
